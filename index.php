a0106366f136419c60a2e3a20b3ffc4c10f6467b
Mauro Mendes Nogueira <mauro@mmsites.com.br>
2020-10-01 11:49:52 -0300

    Alterações no index php

--- index.php
+++ index.php
@@ -6,12 +6,36 @@ $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
-	echo "OK";
-
+$app->get('/', function() {     
+	echo "
+	<center>
+	<h2>
+	<hr>PÁGINA INICIAL <br>
+	<small>Ecommerce Hcode</small>><hr>
+	</h2>
+	</center>
+	";
 });
+
+$app->get('/Home', function() {     
+	echo "
+	<center>
+	<h2>
+	<hr>HOME PAGE<br>
+	<small>Ecommerce Hcode</small>><hr>
+	</h2>
+	</center>
+	";
+});
+
 
 $app->run();
 
  ?>
+
+ <center>
+ <h2>
+ <hr>PÁGINA INICIAL <br>
+ <small>Ecommerce Hcode</small>><hr>
+ </h2>
+ </center>
