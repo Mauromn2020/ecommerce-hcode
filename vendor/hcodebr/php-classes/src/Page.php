915e86282cb595f4fd40e85186159e930f5dc010
Mauro Mendes Nogueira <mauro@mmsites.com.br>
2020-10-02 07:57:51 -0300

    Alterações no index.php e index.html

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -11,7 +11,7 @@
         ];
 
 
-        /** FUNCTION CONSTRUCT CRIA O HEADER DA PÁGINA ******************* */
+        /** CRIA O HEADER DA PÁGINA ******************* */
         public function __construct( $opts = array() ){
 
             //Junta as opções recebidas $opts no construct, com as do array defaults[]
@@ -26,13 +26,30 @@
             Tpl::configure( $config );
 
             $this->tpl = new Tpl;
-
-            $this->setData( $this->options["data"] );
-            
+            $this->setData( $this->options["data"] );            
             $this->tpl->draw("header");
+        }
+        
+
+        /******** CRIA O CONTEÚDO PA PÁGINA ********************** */  
+        public function setTpl( $name,  $data = array(),  $returnHTML = false ){
+            $this->setData($data);
+            return $this->tpl->draw( $name, $returnHTML );
+        }
+        
+
+
+
+
+        /********* CRIA O FOOTER PA PÁGINA ************** */
+        public function __destruct(){
+            $this->tpl->draw("footer"); 
         }
 
 
+
+
+        /********* MÉTODOS DIVERSOS CRIADOS  ************** */
         private function setData($data = array() ){
            foreach ($data as $key => $value) {
                 $this->tpl->assign( $key, $value );
@@ -40,27 +57,6 @@
         }
 
 
-        /******** FUNCTION CRIA O CONTEÚDO PA PÁGINA ********************** */  
-        public function setTpl( $name,  $data = array(),  $returnHTML = false ){
 
-            $this->setData($data);
-
-            return $this->tpl->draw( $name, $returnHTML );
-
-        }
-        
-
-
-
-
-        /********* FUNCTION DESTRUCT CRIA O FOOTER PA PÁGINA ************** */
-        public function __destruct(){
-
-            $this->tpl->draw("footer");     
-
-        }
-
-
-    }
-
+    } // FECHA A CLASSE
 ?>
